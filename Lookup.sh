d9ead2efc2205493a89e491657890cff291a7d92
Rdroid99 <74264262+Rdroid99@users.noreply.github.com>
2020-11-25 18:26:24 +0700

    Add files via upload

---
+++ Lookup.sh
@@ -0,0 +1,36 @@
+#usr/bin/bash
+#DNS Lookup Web by Rdroid99
+#Script kek gini doang mau direcode?:V
+#Gua nub bro:)
+clear
+echo '[               ]0%'
+sleep 1
+clear
+echo '[====           ]25%'
+sleep 1
+clear
+echo '[========       ]50%'
+sleep 1
+clear
+echo '[===========    ]75%'
+sleep 1
+clear
+echo '[===============]100%'
+sleep 1
+clear
+pkg install dnsutils -y
+clear
+neofetch
+echo '
+•••••  ••••••••
+•••••   •••••••••
+•••    •••    ••••
+•••    •••••••••••
+•••    •••••••••
+•••    •••
+•••••  ••• 
+•••••  •••  Dns Lookup v.1'
+echo
+echo 'Example(Contoh):Rdroid99.domainnya'
+echo 'Bila ingin keluar ketik: exit'
+nslookup
